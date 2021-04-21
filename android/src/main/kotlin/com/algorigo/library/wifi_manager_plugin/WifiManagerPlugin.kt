5054619a3e1b6b9c4d8a423f242ad43811163078
Rouddy <rouddy@naver.com>
2021-04-21 10:08:46 +0900

    ui 수정

--- android/src/main/kotlin/com/algorigo/library/wifi_manager_plugin/WifiManagerPlugin.kt
+++ android/src/main/kotlin/com/algorigo/library/wifi_manager_plugin/WifiManagerPlugin.kt
@@ -30,43 +30,49 @@ class WifiManagerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     when (call.method) {
       "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
-      "getConnectedWifiApName" -> {
-        val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as? WifiManager
-        val ssid = wifiManager?.connectionInfo?.ssid
-        if (ssid != null) {
-          result.success(ssid)
-        } else {
-          result.error(UnsupportedOperationException::class.java.simpleName, null, null)
-        }
-      }
-      "connectWifi" -> {
-        val apName = call.argument<String>("apName")
-        val apPassword = call.argument<String>("apPassword")
-        if (apName != null && apPassword != null) {
-          RxWifiManager.connect(context, apName, apPassword)
-                  .subscribe({
-                    result.success(null)
-                  }, {
-                    result.error(it.javaClass.simpleName, it.message, it.stackTraceToString())
-                  })
-        } else {
-          result.error(IllegalArgumentException::class.java.simpleName, null, null)
-        }
-      }
-      "scanWifi" -> {
-        val only24GHz = call.arguments as? Boolean
-        RxWifiManager.scan(context, only24GHz ?: false)
-                .map {
-                  it.map { it.SSID }
-                }
-                .subscribe({
-                  result.success(it);
-                }, {
-                  result.error(it.javaClass.simpleName, it.message, it.stackTraceToString())
-                })
-      }
+      "getConnectedWifiApName" -> getConnectedWifiApName(result)
+      "connectWifi" -> connectWifi(call, result)
+      "scanWifi" -> scanWifi(call, result)
       else -> result.notImplemented()
     }
+  }
+
+  private fun getConnectedWifiApName(result: Result) {
+    val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as? WifiManager
+    val ssid = wifiManager?.connectionInfo?.ssid
+    if (ssid != null) {
+      result.success(ssid)
+    } else {
+      result.error(UnsupportedOperationException::class.java.simpleName, null, null)
+    }
+  }
+
+  private fun connectWifi(call: MethodCall, result: Result) {
+    val apName = call.argument<String>("apName")
+    val apPassword = call.argument<String>("apPassword")
+    if (apName != null && apPassword != null) {
+      RxWifiManager.connect(context, apName, apPassword)
+              .subscribe({
+                result.success(null)
+              }, {
+                result.error(it.javaClass.simpleName, it.message, it.stackTraceToString())
+              })
+    } else {
+      result.error(IllegalArgumentException::class.java.simpleName, null, null)
+    }
+  }
+
+  private fun scanWifi(call: MethodCall, result: Result) {
+    val only24GHz = call.arguments as? Boolean
+    RxWifiManager.scan(context, only24GHz ?: false)
+            .map {
+              it.map { it.SSID }
+            }
+            .subscribe({
+              result.success(it);
+            }, {
+              result.error(it.javaClass.simpleName, it.message, it.stackTraceToString())
+            })
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
